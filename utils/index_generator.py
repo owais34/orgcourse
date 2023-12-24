b17caa8d5c425b74363510a9763de0507261caa5
owais34 <owaishunter78@gmail.com>
2023-12-24 11:41:26 +0530

    added key generator methods

--- utils/index_generator.py
+++ utils/index_generator.py
@@ -1,11 +1,38 @@
 from utils.repository_module import Repository
+from utils.json_utils import Jsoner
+import random
 
+charset = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-class IndexGenerator(Repository,):
+counter = 0
+
+class IndexGenerator(Repository,Jsoner):
     def __init__(self):
         super().__init__()
-        self.indexSet = {}
+        self.indexSet = set()
         dictForm =  self.loadFromRepo()
         if dictForm != None:
             self.indexSet.update(dictForm["indexSet"])
         self.persistChanges()
+
+    def getNewIndex(self, persistInstantly = False):
+        key = self.generateRandomKey()
+        while key in self.indexSet:
+            key = self.generateRandomKey()
+        self.indexSet.add(key)
+        if counter == 0 or persistInstantly:
+            self.persistChanges()
+        counter = (counter + 1) % 10
+        return key
+
+    def generateRandomKey(self) -> str:
+        key = ""
+        
+        for i in range(10):
+            index = random.randrange(0,len(charset))
+            key = key + charset[index]
+        
+        return key
+
+
+    
